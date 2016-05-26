c45b4a7d5016fb63a283513c0aeb51e1943cd7c3
ssenechal67 <ssenechal67@gmail.com>
2016-05-26 14:27:24 -0400

    Add caching support - Clear cache script

--- resources/lib/clearcache.py
+++ resources/lib/clearcache.py
@@ -7,14 +7,13 @@ import xbmcgui
 import xbmcvfs
 from xbmcaddon import Addon
 
-addon = xbmcaddon.Addon()
-addon_name = addon.getAddonInfo('name')
+addon = Addon('plugin.video.telequebec')
 addon_cache_basedir = os.path.join(xbmc.translatePath(addon.getAddonInfo('path')),".cache")
 
 if sys.argv[1].lower() == "full":
-    print "["+addon_name+"TELEQUEBEC] deleting full cache"
+    print "["+addon.getAddonInfo('name')+"] deleting full cache"
     for root, dirs, files in os.walk(addon_cache_basedir):
         for file in files:
-            xbmcvfs.delete( os.path.join( root, file ) )
-    xbmcgui.Dialog().ok(addon.getAddonInfo( 'name' ), "Clean Cache...", "Success")
+            xbmcvfs.delete(os.path.join(root,file))
+    xbmcgui.Dialog().ok(addon.getAddonInfo('name'), addon.getLocalizedString(32108),addon.getLocalizedString(32109))
 
